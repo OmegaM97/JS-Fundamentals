a8022c4054961e52d465af5f6cd08741c159e0d3
Omega Melese <omegamelese68@gmail.com>
2025-05-20 00:01:06 +0300

    finish task 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,16 @@
+const input = process.argv[2];
+const number = parseInt(input);
+
+if (isNaN(number)) {
+  console.log('Missing size');
+} else {
+  let square = '';
+
+  for (let i = 1; i <= number; i++) {
+    for (let o = number; o > 0; o--) {
+      square += "*";
+    }
+    square += "\n";
+  }
+  console.log(square);
+}
