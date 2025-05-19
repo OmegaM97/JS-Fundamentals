c7ef6713029862b3fe4a98837fe588dad45b3638
Omega Melese <omegamelese68@gmail.com>
2025-05-19 23:17:37 +0300

    finish task 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const input = process.argv[2];
+const number = parseInt(input);
+
+if (isNaN(number)) {
+  console.log('Missing number of occurrences');
+} else {
+    for(let i = 1; i <= number; i++) {
+      console.log('C is fun');
+    }
+}
