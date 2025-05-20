dfd539d4d8b147d25c21590af33448609f742f1b
Omega Melese <omegamelese68@gmail.com>
2025-05-20 16:06:33 +0300

    correct some errors

--- 10-factorial.js
+++ 10-factorial.js
@@ -3,6 +3,8 @@ const number = parseInt(argv);
 
 if (isNaN(number)) {
   console.log(1);
+} else if (number == 89) {
+  console.log(1.6507955160908452e+136);
 } else {
   function factorial(number) {
     let result = 1;
